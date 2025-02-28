55c1429193e059f74d7b74546f05fc9f0af7ba6f
João Marcos Alecsandro Kirimis <150600077+jmkirimis@users.noreply.github.com>
2025-02-27 22:34:16 -0300

    Ajustando css da atividade 03

--- ldw-aula-03-atividade-01/controllers/routes.py
+++ ldw-aula-03-atividade-01/controllers/routes.py
@@ -1,17 +1,41 @@
 import urllib.request
 from flask import render_template, request, redirect, url_for
-import urllib, json
+import urllib, json, locale
+
+personagens = []
+charList = []
+
+locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
 
 def init_app(app):
+    @app.template_filter('format_number')
+    def format_number(value):
+        try:
+            return locale.format_string("%d", int(value), grouping=True)
+        except (ValueError, TypeError):
+            return value
+    
     @app.route('/')
     def home():
         return render_template('index.html')
     
-    @app.route('/characters')
+    @app.route('/characters', methods=['GET', 'POST'])
     def characters():
-        return render_template('characters.html')
+        if request.method == 'POST':
+           if request.form.get('personagem'):
+               personagens.append(request.form.get('personagem'))
+               return redirect(url_for('characters'))
+        return render_template('characters.html', personagens=personagens)
     
-    @app.route('/apicharacters', methods=['GET'])
+    @app.route('/cadcharacter', methods=['GET', 'POST'])
+    def cadcharacters():
+        if request.method == 'POST':
+            formdata = request.form.to_dict()
+            charList.append(formdata)
+            return redirect(url_for('cadcharacters'))
+        return render_template('cadcharacters.html', charList=charList)
+    
+    @app.route('/apicharacters', methods=['GET', 'POST'])
     def apicharacters():
         page = request.args.get('page', 1)
         url = f'https://dragonball-api.com/api/characters?page={page}'
