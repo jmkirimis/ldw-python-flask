c602bb4440df5f6df4430b1e44ebfa95791a4fa2
João Marcos Alecsandro Kirimis <150600077+jmkirimis@users.noreply.github.com>
2025-03-17 20:47:21 -0300

    Continuação da aula 04
    Adicionando rotas de exclusão e alteração dos jogos

--- aula-04-crud-com-sqlite/controllers/routes.py
+++ aula-04-crud-com-sqlite/controllers/routes.py
@@ -68,7 +68,16 @@ def init_app(app):
     
     # Rota com CRUD de jogos
     @app.route('/estoque', methods=['GET', 'POST'])
-    def estoque():
+    @app.route('/estoque/delete/<int:id>', methods=['GET', 'POST'])
+    def estoque(id=None):
+        if id:
+            # Selecionando o jogo no banco para ser excluido
+            game = Game.query.get(id)
+            # Deletar o game pela ID
+            db.session.delete(game)
+            db.session.commit()
+            return redirect(url_for('estoque'))
+            
         # Cadastra um novo jogo
         if request.method == 'POST':
             newgame = Game(request.form['titulo'], request.form['ano'], request.form['categoria'], request.form['plataforma'], 
@@ -80,3 +89,20 @@ def init_app(app):
         # Método do SQLAlchemy que faz um select geral no banco na tabela Games
         gamesestoque = Game.query.all()
         return render_template('estoque.html', gamesestoque=gamesestoque)
+
+    # Rota de edição
+    @app.route('/edit/<int:id>', methods=['GET', 'POST'])
+    def edit(id):
+        # Buscando informações do jogo:
+        game = Game.query.get(id)
+        # Edita o jogo com as informações do formulário
+        if request.method == 'POST':
+            game.titulo = request.form['titulo']
+            game.ano = request.form['ano']
+            game.categoria = request.form['categoria']
+            game.plataforma = request.form['plataforma'] 
+            game.preco = request.form['preco']
+            game.quantidade = request.form['quantidade']
+            db.session.commit()
+            return redirect(url_for('estoque'))
+        return render_template('editgame.html', game=game)
